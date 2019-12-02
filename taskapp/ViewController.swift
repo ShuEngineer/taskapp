685703d52324c57fafeeb0559192596ce8854ee4
ShuEngineer <supercar110shu@gmail.com>
2019-12-02 13:25:16 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by 伊藤嵩 on 2019/12/02.
+//  Copyright © 2019 Shu Ito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
