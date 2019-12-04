36d6ff5afbb162ceab8815d0d7ff993cb3a48ad5
ShuEngineer <supercar110shu@gmail.com>
2019-12-04 23:03:32 +0900

    2019/12/04　Realmでタスクを保存する

--- taskapp/InputViewController.swift
+++ taskapp/InputViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import RealmSwift
 
 class InputViewController: UIViewController {
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var contentsTextView: UITextView!
     @IBOutlet weak var datePicker: UIDatePicker!
     
+    let realm = try! Realm()
+    var task: Task!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //背景をタップしたらdismissKeyboardメソッドを呼ぶように設定する
+        //タップを判断するにはUITapGestureRecognizerクラスを利用
+        //UITapGestureRecognizerクラスの初期化時にタップされたときにどのクラスのどのメソッドが呼ばれるかを指定
+        //クラス：self(=InputViewController自身)
+        //メソッド：dismissKeyboard()メソッドを指定
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target:self, action:#selector(dismissKeyboard))
+        //viewプロパティが背景に該当するので、addGestureRecognizer(_:)メソッドを使って
+        //viewにUITapGestureRecognizerを登録
+        self.view.addGestureRecognizer(tapGesture)
+        
+        //UIに値を反映するために最初にUIを作成した時に設定したアウトレットにそれぞれの値を設定
+        titleTextField.text = task.title
+        contentsTextView.text = task.contents
+        datePicker.date = task.date
+    }
+    @objc func dismissKeyboard(){
+        //dismissKeyboard()メソッドでendEditing(true)を呼び出してキーボードを閉じる
+        view.endEditing(true)
     }
     
-
+    //viewWillDisappear(_:)メソッド 遷移する際に、画面が非表示になるとき呼ばれるメソッド
+    //Realmのwrite(_:)メソッドを使用
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        try! realm.write {
+            self.task.title = self.titleTextField.text!
+            self.task.contents = self.contentsTextView.text
+            self.task.date = self.datePicker.date
+            self.realm.add(self.task, update: true)
+        }
+        
+        super.viewWillDisappear(animated)
+    }
+    
     /*
     // MARK: - Navigation
 
--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -7,9 +7,19 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
+    
+    //Realmインスタンスを取得する
+    let realm = try! Realm()
+    
+    // DB内のタスクが格納されるリスト。データの配列
+    // 日付近い順\順でソート：降順 ascending: false
+    // 以降内容をアップデートするとリスト内は自動的に更新される。
+    var taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending: false)
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,13 +30,25 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     // MARK: UITableViewDataSourceプロトコルのメソッド
     // データの数（＝セルの数）を返すメソッド
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return taskArray.count  //データの数＝配列の数
     }
     
     // 各セルの内容を返すメソッド
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // 再利用可能な cell を得る
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        //cellに値を設定する
+        //データの配列であるtaskArrayから該当するデータを取り出してセルに設定
+        let task = taskArray[indexPath.row]
+        cell.textLabel?.text = task.title
+        
+        //DateFormatterクラスは日付を表すDateクラスを任意の形の文字列に変換する機能を持つ
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        
+        let dateString:String = formatter.string(from: task.date)
+        cell.detailTextLabel?.text = dateString
         
         return cell
     }
@@ -47,7 +69,54 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     // Delete ボタンが押された時に呼ばれるメソッド
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            // データベースから削除する
+            try! realm.write {
+                self.realm.delete(self.taskArray[indexPath.row])
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+        }
     }
-
+    //タスク一覧画面からタスク作成/編集画面に遷移する時にデータであるTaskクラスを渡す処理
+   //画面遷移自体は先に実装していたperformSegue(withIdentifier:sender:)メソッドによって
+   //cellSegueのsegueが実行されて画面遷移するが、『＋』ボタンとセルをタップした時の２つのケースで遷移することがあるので
+   //このメソッドで場合分する
+    //segueで画面遷移する時に呼ばれる
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //セルをタップした時は先ほど設定したIdentifierがcellSegueであるsegueが発行
+        //dentifierがcellSegueのときはすでに作成済みのタスクを編集するとき
+        //配列taskArrayから該当するTaskクラスのインスタンスを取り出してinputViewControllerのtaskプロパティに設定
+        let inputViewController:InputViewController = segue.destination as! InputViewController
+        
+        if segue.identifier == "cellSegue" {
+            let indexPath = self.tableView.indexPathForSelectedRow
+            inputViewController.task = taskArray[indexPath!.row]
+        }else{
+            
+            //+ボタンをタップした時はTaskクラスのインスタンスを生成して、初期値として現在時間と、
+            //プライマリキーであるIDに値を設定
+            //taskArray.max(ofProperty: "id")ですでに存在しているタスクのidのうち
+            //最大のものを取得し、1を足すことで他のIDと重ならない値を指定
+            let task = Task()
+            task.date = Date()
+            
+            let allTasks = realm.objects(Task.self)
+            if allTasks.count != 0 {   // != not equal
+                task.id = allTasks.max(ofProperty: "id")! + 1
+            }
+            
+            inputViewController.task = task
+        }
+    }
+    
+    //タスク作成/編集画面から戻ってきた時に画面(TableView)を更新する処理
+    //viewWillAppear:メソッドを追加し、UITableViewクラスのreloadDataメソッドを呼ぶことで
+    //タスク作成/編集画面で新規作成/編集したタスクの情報をTableViewに反映させる
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
 }
 
